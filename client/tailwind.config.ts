88e35f3bf0ef4f3fb6e8b22120095d8ae38c8382
Nathan <ngothuan2422001@gmail.com>
2023-08-23 16:59:33 +0700

    init

---
+++ client/tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    screens: {
+      xs: "480px",
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      xxl: "1600px",
+      "3xl": "1700px",
+      "4xl": "1800px",
+    },
+  },
+  plugins: [],
+}
+export default config
