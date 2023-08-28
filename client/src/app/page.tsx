77483ee65a7758aa12b8f82277dfdccb36f2b2d1
Nathan <ngothuan2422001@gmail.com>
2023-08-28 17:48:38 +0700

    update

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,14 @@
+'use client'
+
+import { signOut, useSession } from 'next-auth/react'
 
 export default function Home() {
+  const { data: session, status } = useSession({ required: true })
+
   return (
-    <main></main>
+    <div>
+      <p>{session?.user?.email}</p>
+      <button onClick={() => signOut()}>Logout</button>
+    </div>
   )
 }
