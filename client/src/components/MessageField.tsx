f17c8425ec26109a084ae6568a4a0345daaeb466
Nathan <ngothuan2422001@gmail.com>
2023-09-04 13:12:01 +0700

    Done

--- client/src/components/MessageField.tsx
+++ client/src/components/MessageField.tsx
@@ -4,7 +4,7 @@ import { socket } from '@/socket'
 import moment from 'moment'
 import { useSession } from 'next-auth/react'
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export interface IMessageField {
   conversationId: string
@@ -14,6 +14,11 @@ export function MessageField({ conversationId }: IMessageField) {
   const { data: session } = useSession({ required: true })
   const currentEmail = session?.user?.email as string
   const [messagesRecieved, setMessagesReceived] = useState<IMessage[]>([])
+  const messagesRef = useRef<null | HTMLDivElement>(null)
+
+  const scrollToBottom = () => {
+    messagesRef.current?.scroll({ top: messagesRef.current?.scrollHeight })
+  }
 
   useEffect(() => {
     const getMessages = async () => {
@@ -26,6 +31,7 @@ export function MessageField({ conversationId }: IMessageField) {
   useEffect(() => {
     socket.on('receive_message', (data) => {
       setMessagesReceived((state) => [...state, { ...data }])
+      scrollToBottom()
     })
 
     return () => {
@@ -33,8 +39,15 @@ export function MessageField({ conversationId }: IMessageField) {
     }
   }, [])
 
+  useEffect(() => {
+    scrollToBottom()
+  }, [messagesRecieved])
+
   return (
-    <div className="messages flex h-full flex-col items-center overflow-y-scroll">
+    <div
+      ref={messagesRef}
+      className="messages flex h-full flex-col items-center overflow-y-scroll"
+    >
       {messagesRecieved.map((item) => {
         const sendTime = moment(item.__createdtime__).calendar()
         return (
